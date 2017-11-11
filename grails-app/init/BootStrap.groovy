34d55b8e20559d8714461d20ba3113e837e29644
Chang <chang@Changs-MacBook-Pro.local>
2017-11-11 09:00:08 +0800

    3

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,5 +1,7 @@
+import grails.config.Config
 import grails.core.GrailsApplication
 import org.springframework.cloud.config.server.environment.EnvironmentRepository
+import org.yaml.snakeyaml.Yaml
 
 class BootStrap {
 
@@ -7,24 +9,44 @@ class BootStrap {
 
     GrailsApplication grailsApplication
 
+    Map<String, String> mergedMap = new HashMap<>()
+
+    Yaml yaml = new Yaml()
+
+
     def init = { servletContext ->
-        List list = environmentRepository.findOne("ciab", "tw", null).getPropertySources()
+        List list = environmentRepository.findOne("ciab", "tw_stating", null).getPropertySources()
 
         for (int i = list.size()-1; i>=0; i--){
             println list.get(i).name
-            list.get(i).getSource().each {k,v ->
+            list.get(i).getSource().each {k,v->
                 if (v instanceof String){
-                    println k
-                    println v
+                    mergedMap.put(k,v)
                 }
-
             }
-            grailsApplication.config.merge(list.get(i).getSource())
+            mergedMap.putAll(list.get(i).getSource())
+
         }
+
+        mergedMap.sort()
+
+        grailsApplication.config.merge(mergedMap)
+
+
+
     }
 
     def destroy = {
     }
 
+
+    void printMap(Map map){
+        map.each { k, v ->
+            if (v instanceof String) {
+                println k
+                println v
+            }
+        }
+    }
 
 }
--- grails-app/init/grailsone/Application.groovy
+++ grails-app/init/grailsone/Application.groovy
@@ -2,7 +2,9 @@ package grailsone
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.cloud.config.server.EnableConfigServer
 
+@EnableConfigServer
 class Application extends GrailsAutoConfiguration {
 
     static void main(String[] args) {
